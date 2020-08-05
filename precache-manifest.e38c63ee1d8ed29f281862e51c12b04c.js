5806ea0fb8d3cfff5fffd96bc9b9f2bea2abcbc0
Aditya <pratapsinghaditya2001@gmail.com>
2020-08-05 22:57:39 +0530

    Updates

---
+++ precache-manifest.e38c63ee1d8ed29f281862e51c12b04c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db1928b0a533022f07b0218ee8496b91",
+    "url": "/React_BreakingBadAPI/index.html"
+  },
+  {
+    "revision": "e3faafd1ebff38e571e7",
+    "url": "/React_BreakingBadAPI/static/css/main.95e9bd26.chunk.css"
+  },
+  {
+    "revision": "764bcd727290526f7c1a",
+    "url": "/React_BreakingBadAPI/static/js/2.8f1af6ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React_BreakingBadAPI/static/js/2.8f1af6ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3faafd1ebff38e571e7",
+    "url": "/React_BreakingBadAPI/static/js/main.64d5d0e9.chunk.js"
+  },
+  {
+    "revision": "237e847bc5b395063222",
+    "url": "/React_BreakingBadAPI/static/js/runtime-main.55e4b75a.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/React_BreakingBadAPI/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/React_BreakingBadAPI/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/React_BreakingBadAPI/static/media/spinner.235b9b3e.gif"
+  }
+]);
